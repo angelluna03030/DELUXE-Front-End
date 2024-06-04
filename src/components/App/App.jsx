ada1444ec00871b2ae70122b08564b7d85aa75da
angel luna <angelstivengarcialuna@gmail.com>
2024-06-04 10:07:45 -0500

    agregando enrutamiento y alertas

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return <></>;
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import {Rutas} from "../../routes"
+export default function App() {
+  return (
+    <>
+  <BrowserRouter>
+		<Rutas></Rutas>
+			<ToastContainer />
+		</BrowserRouter>
+    </>
+  );
 }
-
-export default App;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+
+
+export const Rutas = () => {
+	return (
+		<Routes>
+			{/* <Route path='/' element={<Inicio />} /> */}
+		</Routes>
+	);
+};

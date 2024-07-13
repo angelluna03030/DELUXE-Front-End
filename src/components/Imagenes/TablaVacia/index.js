f5bb7756701b6c754e2a8f813153d446d99ade47
angel luna <angelstivengarcialuna@gmail.com>
2024-07-12 23:59:12 -0500

    Tabla vacia leader

---
+++ src/components/Imagenes/TablaVacia/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Spinner} from "@nextui-org/react";
+const TablaCargandoAnimacion = () => (
+	<div
+		style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: '100%',
+		}}
+	>
+		<Spinner />
+	</div>
+);
+
+export default TablaCargandoAnimacion;

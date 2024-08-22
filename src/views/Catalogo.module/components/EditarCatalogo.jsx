923745f9dd7f2ae05c4921f1422c935bef12b627
angel luna <angelstivengarcialuna@gmail.com>
2024-08-21 21:51:24 -0500

    los madlas para poder editar el contenido del catalgo o

--- src/views/Catalogo.module/components/EditarCatalogo.jsx
+++ src/views/Catalogo.module/components/EditarCatalogo.jsx
@@ -1,5 +1,12 @@
-export const EditarCatalogo =()=>{
-    return <>
-    
-    </>
-}
+import { EditarImagenesParaVideo, EditarVideo, EditarGaleriaDeImagenes, EditarProductosDestacados } from './index';
+
+export const EditarCatalogo = () => {
+  return (
+    <div className='grid grid-cols-2 sm:grid-cols-4 gap-4 w-full max-w-7xl mx-auto my-5'>
+      <EditarImagenesParaVideo />
+      <EditarVideo />
+      <EditarGaleriaDeImagenes />
+      <EditarProductosDestacados />
+    </div>
+  );
+};
--- src/views/Catalogo.module/components/TablaCatalogo.jsx
+++ src/views/Catalogo.module/components/TablaCatalogo.jsx
@@ -5,20 +5,18 @@ import { Producto } from '../../../components/Producto';
 import { Categorias } from '../../../components/Categorias';
 
 import { GaleriaImagenes } from '../../../components/GaleriadeImagenes';
-import {EditarCatalogo} from "./index"
+import { EditarCatalogo } from './index';
 export const TablaCatalogo = () => {
   return (
     <div className='flex flex-col gap-6'>
       <EditarCatalogo></EditarCatalogo>
       <div className='sm:flex'>
-      <Layout />
-
+        <Layout />
       </div>
       <Carrusel />
       <Categorias />
-      <div className='w-80 h-80 justify-center items-center mb-64 ml-20  sm:ml-96'>
-      <VideoPlayer />
-
+      <div className='w-80 h-80 justify-center items-center mb-64 ml-14  sm:ml-96'>
+        <VideoPlayer />
       </div>
       <GaleriaImagenes />
       <Producto />

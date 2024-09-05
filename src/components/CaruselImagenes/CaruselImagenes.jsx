2c8f853641a83aaf6c6caff7ddfe85be8e97cc90
angel luna <angelstivengarcialuna@gmail.com>
2024-09-05 00:37:00 -0500

    Carrusel Arreglando problemas en lo del moviles

--- src/components/CaruselImagenes/CaruselImagenes.jsx
+++ src/components/CaruselImagenes/CaruselImagenes.jsx
@@ -2,24 +2,47 @@ import { useState, useEffect } from 'react';
 import imagen_No_funtion from '../../assets/no-fotos.png';
 const RUTA_API = import.meta.env.VITE_API_URL;
 import { Skeleton } from '@nextui-org/react';
-// Componente Carrusel que acepta un array de imágenes como prop
+
 export const Carrusel = ({ imagenes = [] }) => {
   const [offset, setOffset] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   useEffect(() => {
     const handleAnimation = () => {
       setOffset(prevOffset => (prevOffset >= 100 ? 0 : prevOffset + 0.1));
     };
 
-    const intervalId = setInterval(handleAnimation, 20); // Ajusta el valor para la velocidad deseada
+    const intervalId = setInterval(handleAnimation, 20);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  if (!imagenes.length) {
+  // Check if all images are loaded
+  useEffect(() => {
+    if (imagenes.length === 0) return;
+
+    let loadedImagesCount = 0;
+
+    const onLoad = () => {
+      loadedImagesCount += 1;
+      if (loadedImagesCount === imagenes.length) {
+        setImagesLoaded(true);
+      }
+    };
+
+    imagenes.forEach(imagenSrc => {
+      const img = new Image();
+      img.src = `${RUTA_API}/public/${imagenSrc}`;
+      img.onload = onLoad;
+      img.onerror = onLoad; // Handle errors by considering the image loaded
+    });
+
+  }, [imagenes]);
+
+  if (!imagesLoaded) {
     return (
       <div className='overflow-hidden rounded-lg shadow-md relative h-96 w-80 mx-auto mt-10'>
-        <Skeleton className='rounded-lg  sm:m-5  h-96 w-80 m-auto mb-10' />;
+        <Skeleton className='rounded-lg sm:m-5 h-96 w-80 m-auto mb-10' />;
       </div>
     );
   }
@@ -32,7 +55,7 @@ export const Carrusel = ({ imagenes = [] }) => {
   };
 
   return (
-    <div className='overflow-hidden rounded-lg shadow-md relative h-96 w-80 mx-auto mt-10 '>
+    <div className='overflow-hidden rounded-lg shadow-md relative h-96 w-80 mx-auto mt-10'>
       <div className='flex' style={containerStyle}>
         {imagenes.map((imagenSrc, index) => (
           <div

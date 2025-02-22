498cabf0bb8f438fc089689fa8fc5a6f0d9fc6ac
angel luna <angelstivengarcialuna@gmail.com>
2025-02-21 22:18:03 -0500

    Ya la pude migrar bien

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './components/App';
-import { NextUIProvider } from '@nextui-org/react';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import { NextUIProvider } from "@nextui-org/react";
+import "./index.css";
 
-// Redirección a HTTPS
-// if (window.location.protocol !== 'https:') {
-//   window.location.href = `https://${window.location.host}${window.location.pathname}`;
-// }
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
       <App />
     </NextUIProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+
+  ],
+  css: {
+    postcss: "./postcss.config.js"
+  },
+  
+  define: {
+    'process.env': {}, // Esto crea un objeto vacío para 'process.env'
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  
+  optimizeDeps: {
+    include: ['agent-base', 'proxy-agent'],
+  },
+  build: {
+    sourcemap: true, // Usa esta propiedad directamente
+    rollupOptions: {
+      input: 'index.html',
+    },    
+    outDir: 'dist',
+  },
+});

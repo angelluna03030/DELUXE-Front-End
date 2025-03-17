8b6e0fecb725317357feb49ce949044bfac5274b
angel luna <angelstivengarcialuna@gmail.com>
2025-03-17 00:14:47 -0500

    mejoras en el buscar

--- src/components/Header/Busqueda.tsx
+++ src/components/Header/Busqueda.tsx
@@ -9,7 +9,7 @@ export const Busqueda = () => {
   const navigate = useNavigate();
   const [consulta, setConsulta] = useState('');
   const [items, setItems] = useState<any[]>([]);
-  const [isSearchOpen, setIsSearchOpen] = useState(true); // Estado para manejar la visibilidad
+  const [isSearchOpen, setIsSearchOpen] = useState(true);
 
   const fetchProductos = async (searchTerm = '') => {
     try {
@@ -17,13 +17,33 @@ export const Busqueda = () => {
       const response = await getData(url);
 
       if (response.status !== 200) {
-        if (response.status === 401) {
-          console.error("No autorizado: Verifique sus credenciales.");
-        } else {
-          console.error("Error al buscar productos:", response.status);
-        }
+        console.error("Error al buscar productos:", response.status);
         return;
       }
+
+      const data = Array.isArray(response.dataResponse) ? response.dataResponse : [];
+
+      if (data.length > 0) {
+        setItems(data);
+      } else {
+        // Si no encuentra productos, intenta con la búsqueda avanzada
+        fetchProductosPotente(searchTerm);
+      }
+    } catch (error) {
+      console.error('Error al buscar productos:', error);
+    }
+  };
+
+  const fetchProductosPotente = async (searchTerm = '') => {
+    try {
+      const url = `${RUTA_API}/api/productos/mejorbuscador/${searchTerm}`;
+      const response = await getData(url);
+
+      if (response.status !== 200) {
+        console.error("Error al buscar productos:", response.status);
+        return;
+      }
+
       setItems(Array.isArray(response.dataResponse) ? response.dataResponse : []);
     } catch (error) {
       console.error('Error al buscar productos:', error);
@@ -45,8 +65,8 @@ export const Busqueda = () => {
 
   const handleSelectionChange = (item: { value: string }) => {
     if (item && item.value) {
-      navigate(`/productos/buscar/${item.value}`);
-      setIsSearchOpen(false); // Cerrar modal al seleccionar un producto
+      navigate(`/producto/${item.value}`);
+      setIsSearchOpen(false);
     }
   };
 
@@ -58,46 +78,38 @@ export const Busqueda = () => {
     e.preventDefault();
     if (consulta.trim()) {
       navigate(`/productos/buscar/${consulta}`);
-      setIsSearchOpen(false); // Cerrar modal al hacer la búsqueda
+      setIsSearchOpen(false);
     }
   };
 
-  if (!isSearchOpen) return null; // Ocultar modal si está cerrado
+  if (!isSearchOpen) return null;
 
   return (
-<div className="fixed inset-0 flex items-start justify-center  bg-gray-900 bg-opacity-50 backdrop-blur-md transition-opacity z-50 ">
-  <form
-    onSubmit={handleSubmit}
-    className="bg-white flex p-6 shadow-xl  w-full  relative animate-fade-in mx-auto"
-  >
-    <Autocomplete
-      label="Búsqueda"
-      variant="bordered"
-      allowsCustomValue
-      onSelectionChange={(key) => {
-        const selectedItem = items.find(item => item._id === key);
-        if (selectedItem) {
-          handleSelectionChange({ value: selectedItem._id });
-        }
-      }}
-      onInputChange={handleInputChange}
-      className="w-11/12 "
-    >
-      {items.map((item) => (
-        <AutocompleteItem key={item._id} value={item._id}>
-          {item.nombreproductos}
-        </AutocompleteItem>
-      ))}
-    </Autocomplete>
-    <button
-      type="button"
-      onClick={toggleSearch}
-      className="mx-2 m-auto  text-black text-xl transition"
-    >
-      ✖
-    </button>
-  </form>
-</div>
-
+    <div className="fixed inset-0 flex items-start justify-center bg-gray-900 bg-opacity-50 backdrop-blur-md transition-opacity z-50">
+      <form onSubmit={handleSubmit} className="bg-white flex p-6 shadow-xl w-full relative animate-fade-in mx-auto">
+        <Autocomplete
+          label="Búsqueda"
+          variant="bordered"
+          allowsCustomValue
+          onSelectionChange={(key) => {
+            const selectedItem = items.find(item => item._id === key);
+            if (selectedItem) {
+              handleSelectionChange({ value: selectedItem._id });
+            }
+          }}
+          onInputChange={handleInputChange}
+          className="w-11/12"
+        >
+          {items.map((item) => (
+            <AutocompleteItem key={item._id} value={item._id}>
+              {item.nombreproductos}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete>
+        <button type="button" onClick={toggleSearch} className="mx-2 m-auto text-black text-xl transition">
+          ✖
+        </button>
+      </form>
+    </div>
   );
 };
